e3c973a86c5ae2b80e42b5ad6d7623bfb0581722
Tatyana Molchanova <tatuka.08@mail.ru>
2021-11-29 20:11:24 +0300

    changed howMuchMinutes, added resAnswer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val countSec = 85610
+    val countSec = 1010
 
     val result = agoToText(countSec)
     println(result)
@@ -9,14 +9,10 @@ fun agoToText(countSec: Int): String {
     return when (countSec) {
         in 0..60 -> "был(а) только что"
         in 61..60 * 60 -> {
-            val timeLast = countSec / 60
-            val minutes = howMuchMinutes(timeLast)
-            "был(а) $timeLast $minutes назад"
+            resAnswer(countSec, "minutes")
         }
         in 60 * 60 + 1..24 * 60 * 60 -> {
-            val timeLast = countSec / (60 * 60)
-            val hours = howMuchHours(timeLast)
-            "был(а) $timeLast $hours назад"
+            resAnswer(countSec, "hours")
         }
         in 24 * 60 * 60 + 1..2 * 24 * 60 * 60 -> "был(а) сегодня"
         in 2 * 24 * 60 * 60 + 1..3 * 24 * 60 * 60 -> "был(а) вчера"
@@ -25,14 +21,14 @@ fun agoToText(countSec: Int): String {
 }
 
 fun howMuchMinutes(countMinutes: Int): String {
-    return when (countMinutes) {
-        1, 21, 31, 41, 51 -> "минуту"
-        2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54 -> "минуты"
+    return when {
+        (countMinutes == 1 || countMinutes % 10 == 1) && countMinutes != 11 -> "минуту"
+        (countMinutes % 10 in 2..4) && (countMinutes !in 12..14) -> "минуты"
         else -> "минут"
     }
 }
 
-fun howMuchHours(countHours: Int): Any {
+fun howMuchHours(countHours: Int): String {
     val countHoursStr = countHours.toString()
     val countMinutesLastChar = countHoursStr.substring(countHoursStr.length - 1, countHoursStr.length)
 
@@ -53,5 +49,17 @@ fun howMuchHours(countHours: Int): Any {
             "2", "3", "4" -> "часа"
             else -> "часов"
         }
+    }
+}
+
+fun resAnswer(countSec: Int, typeOfTime: String): String {
+    return if (typeOfTime == "minutes") {
+        val timeLast = countSec / 60
+        val minutes = howMuchMinutes(timeLast)
+        "был(а) $timeLast $minutes назад"
+    } else {
+        val timeLast = countSec / (60 * 60)
+        val hours = howMuchHours(timeLast)
+        "был(а) $timeLast $hours назад"
     }
 }
